614012e7d12edb0ed20d5fed0c25ff37f3f83606
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2023-05-09 18:34:44 -0400

    updated guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,49 +1,36 @@
-// io : input/ouput library. std - standard library.
-use std::io;
+
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
-// fn - declares the new function
-
-fn main(){
-    // println! is a macro
-    println!("Guessing the number");
-
-    // rand::thread_rng function that generates random number.
-    // gen_range takes expression as an argument that generates the number.
-    // start..=end (1 to 100)-- is inclusive on the lower and uppper bounds.
+fn main() {
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-    // instantiating new variable guess 
-    // creates the variable mutable.
-    // = binds a variable with something.
-    // String::new() -> new instance of String.
-    // :: associates the new function with std library.
-
-    let mut guess = String::new();
-
-    // recalling the io library.
-    // handle userinput from io: std - input()
- 
-    io::stdin()
-        // read_line is a method.
-        // &mut guess - is a argument. with variable type &mut.
-        // & - indicates argument is a reference.
-        // it lets code to access without copying data into memory multiple times.
-        .read_line(&mut guess) //returns result value.
-        // result value is an enumeration with each possible state as variant. [Ok and Err]
-        
-        // handles the exceptiong or errors.
-        .expect("Failed to read line");
-
-    // 
-    println!("You guessed: {guess}");
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/aashishtangnami/Documents/Projects/RUST/Rust/guessing_game/target/debug/guessing_game: /Users/aashishtangnami/Documents/Projects/RUST/Rust/guessing_game/src/main.rs
